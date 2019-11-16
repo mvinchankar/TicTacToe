9348e2d6a9b1be9ee346b105ebedcf81b1676f64
mvinchankar <mvinchankar18@gmail.com>
2019-11-16 15:44:40 +0530

    ADDED letter is assigned to Player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 echo "*************************Welcome to TICTACTOE WORLD****************************"
 declare -a boardOfTicTacToe
+PLAYER="X"
+COMPUTER="O"
 function resetTheBoard()
 {
  for (( i=1; i<=9; i++ ))
